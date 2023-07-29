f3e4d76581bd8434bd8073ddeeb9f5b917e177a9
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-07-29 19:18:55 -0300

    feat: add FormLogin components and request login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
-import SignUpPage from '../pages/signUp';
-
-
-
-
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "../pages/signUp";
+import LoginPage from "../pages/login";
 
 export const Router = () => (
   <Routes>
-    <Route path="/" element={<SignUpPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<SignUpPage />} />
   </Routes>
 );
