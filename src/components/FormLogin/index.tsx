6b04f3a1c2bdcbd61cb7c56d17ed0d07128385a7
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-07-30 05:16:27 -0300

    feat:add styles login page and components

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -5,6 +5,8 @@ import Input from "../Input";
 import { ClientContext } from "../../provides/ClientContext";
 import { IClient } from "../../provides/ClientContext/@types";
 import { schema } from "./schema";
+import StyledFormContainerLogin from "./style";
+import { Link } from "react-router-dom";
 
 export const FormLogin = () => {
   const { loginClient } = useContext(ClientContext);
@@ -18,30 +20,38 @@ export const FormLogin = () => {
   });
 
   const submit: SubmitHandler<IClient> = (data) => {
-    console.log(data)
-    loginClient(data)
+    console.log(data);
+    loginClient(data);
   };
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      <Input
-        label="Email"
-        type="text"
-        placeholder="Seu Email"
-        text="Email Address"
-        id="email"
-        register={register("email")}
-      />
-      <p>{errors.email?.message}</p>
-      <Input
-        label="Password"
-        type="password"
-        placeholder="Password"
-        text="Password"
-        id="password"
-        register={register("password")}
-      />
-      <p>{errors.password?.message}</p>
-      <button type="submit">Sign in</button>
-    </form>
+    <StyledFormContainerLogin>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit(submit)}>
+        <Input
+          label="Email"
+          type="text"
+          placeholder="Seu Email"
+          text="Email"
+          id="email"
+          register={register("email")}
+        />
+        <span>{errors.email?.message}</span>
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Sua senha"
+          text="Senha"
+          id="password"
+          register={register("password")}
+        />
+        <span>{errors.password?.message}</span>
+        <button type="submit">Entrar</button>
+        <span className="span-redirect-register">Ainda nao possui uma conta ?</span>
+        <div className="container-button-signUp">
+          {/* POSSIVEL COMPONENTE */}
+          <Link to="/register">Cadastrar-se</Link>
+        </div>
+      </form>
+    </StyledFormContainerLogin>
   );
 };
